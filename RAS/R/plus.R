68d8a3585c50ba82ae7a29d81fcc56b900e627c0
unknown <Clara.Schartner@gmail.com>
2015-11-16 12:05:17 +0100

    Tada!

---
+++ RAS/R/plus.R
@@ -0,0 +1,15 @@
+#'Plus
+#'
+#'Adding numbers!
+#'Bigger number has to come first
+#' 
+#'
+#'@param a larger number
+#'@param b smaller number
+#'@return added values
+#'
+
+
+plus<-function(a,b){
+  2*a-(a-b)
+}
